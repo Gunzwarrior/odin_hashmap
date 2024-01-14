e65b20a51aac6066f1daef07945f90007e1af28c
Thomas Rialland <rialland.thomas@hotmail.fr>
2024-01-14 10:42:56 +0100

    Create hash method

--- main.rb
+++ main.rb
@@ -1,7 +1,19 @@
 class Hashmap
 
-  def hash
-    
+  attr_reader :capacity
+
+  def initialize
+    @capacity = 16
+    @load_factor = 0
+  end
+
+  def hash(string)
+    hash_code = 0
+    prime_number = 31
+
+    string.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+
+    hash_code % capacity
   end
 
   def set
