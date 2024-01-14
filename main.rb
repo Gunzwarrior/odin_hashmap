ae25e650452cbde773b40fc2512feca500fd1009
Thomas Rialland <rialland.thomas@hotmail.fr>
2024-01-14 10:33:01 +0100

    Lay out each method to implement

---
+++ main.rb
@@ -0,0 +1,43 @@
+class Hashmap
+
+  def hash
+    
+  end
+
+  def set
+    
+  end
+
+  def get
+    
+  end
+
+  def key?
+    
+  end
+
+  def remove
+    
+  end
+
+  def length
+    
+  end
+
+  def clear
+    
+  end
+
+  def keys
+    
+  end
+
+  def values
+    
+  end
+
+  def entries
+    
+  end
+
+end
