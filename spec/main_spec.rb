8af1c98030e381a3f256742c04bda0d8022d07b0
Thomas Rialland <rialland.thomas@hotmail.fr>
2024-01-14 11:23:48 +0100

    Create tests for the hash method

---
+++ spec/main_spec.rb
@@ -0,0 +1,15 @@
+require './main'
+
+describe Hashmap do
+  describe "#hash" do
+    it "takes a string and returns a number" do
+      map = Hashmap.new
+      expect(map.hash("test")).to eql(2)
+    end
+
+    it "takes another string and returns a number" do
+      map = Hashmap.new
+      expect(map.hash("rabbit")).to eql(10)
+    end
+  end
+end
